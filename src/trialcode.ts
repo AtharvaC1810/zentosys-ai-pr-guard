d6b964860242b232995e463be4027ce9db8c0faf
AtharvaC1810 <saplatharva@gmail.com>
2025-12-30 12:33:37 +0530

    added wrong code pass

--- src/trialcode.ts
+++ src/trialcode.ts
@@ -1,7 +1,8 @@
 const password = "admin123"; // hardcoded secret
+console.log(password); // 👈 makes ESLint happy
 
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
-console.log(add(2, "3"))
+console.log(add(2, "3"));
